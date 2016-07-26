8bba1d7e3b0e57a30de3b95f69213515864fb4ba
liuweiqin <772863869@qq.com>
2016-07-26 17:12:06 +0800

    多次循序渐进的讲解

---
+++ react-commont/public/scripts/tutorial8.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ruiyou on 2016/7/26.
+ */
+
+var data = [
+    {author: "Pete Hunt", text: "This is one comment"},
+    {author: "Jordan Walke", text: "This is *another* comment"}
+];
+
---
+++ reactjs/public/scripts/tutorial11.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ruiyou on 2016/7/26.
+ */
+
+ReactDOM.render(
+  <CommentBox url="/api/comments" />,
+  document.getElementById('content')
+);
