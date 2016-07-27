cf6e435ef3cd1cec3fadaa09735793196f9af672
liuweiqin <772863869@qq.com>
2016-07-27 21:22:54 +0800

    回调函数，提交到服务器

--- react-commont/public/scripts/tutorial17.js
+++ react-commont/public/scripts/tutorial17.js
@@ -10,10 +10,10 @@ var CommentBox = React.createClass({
            cache: false,
            success: function (data) {
                this.setState({data: data});
-           },
+           }.bind(this),
            error: function (xhr, status, err) {
                console.error(this.props.url, status, err.toString());
-           }
+           }.bind(this)
        });
    },
     handleCommentSubmit: function () {
